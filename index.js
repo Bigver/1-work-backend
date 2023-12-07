d853d9976b1ff3e7bf066d2a421cf87a444b02e8
Bigver <0972356484a@gmail.com>
2023-12-07 18:27:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRouter from "./Routers/userRoutes.js";
+import actionrRouter from "./Routers/actionRoutes.js";
+import uploadRouter from "./Routers/uplodeRoutes.js";
+
+
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+
+const port = 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+app.use("/api/post", actionrRouter);
+app.use("/api/upload", uploadRouter);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
